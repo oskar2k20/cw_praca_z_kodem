c873a5073dfc51eecc559548688c8a8a177d42c7
oskar2k20 <103363025+oskar2k20@users.noreply.github.com>
2022-05-08 11:46:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 def hello():
     """funkcja lint"""  
     return "<h1>Hello WSB! Greetings from Flask!</h1>"
-  
 if __name__ == "__main__":
- app.run(debug=True)
+    app.run(debug=True)
